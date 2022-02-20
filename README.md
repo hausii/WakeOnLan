8c3cd19e0191d413b5aadf98ffd18f57270504f0
hausii <40995793+hausii@users.noreply.github.com>
2022-02-20 13:04:10 +0100

    Added YAML configuration file
    Signed-off-by: hausii <40995793+hausii@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
+WOL Python3
+===========
+
 This Python3 script is used to remotely wake up computers on the local network.
 
-You can specify the MAC address and execute the python script to wake up the computer on the local network. The script
+You can specify the MAC address and execute the python script to wake up the computer on the local network.
 
 The script is based on an ActiveState recipe and was updated for Python3
-https://code.activestate.com/recipes/users/2143621/
+https://code.activestate.com/recipes/users/2143621/ and was extened with a YAML configuration file where you can specify multiple computers.
+
+Usage
+-----
+
+  wol.py
+
+Configuration
+-------------
+
+A wol_config.yml file must be saved in the same directory as wol.py. It contains the computer configurations for all the hosts that should be woken up. I have included a wol_config_sample.yml you can rename and moifiy with your hosts. For each host the name and MAC address must be defined. The MAC address can either be with seperators (:,-) or without.
+
+1:
+    hostname: XXX
+    MAC_address: XX:XX:XX:XX:XX:XX
+
+License
+-------
+Copyright (c) Hannes Hausegger, released under MIT License, see [LICENSE](LICENSE)
--- wol.py
+++ wol.py
@@ -3,27 +3,28 @@
 
 import socket
 import struct
+import yaml
 
 
-def wake_on_lan(macaddress):
+def wake_on_lan(MAC_address):
     """ Switches on remote computers using WOL. """
 
-    # Check macaddress format and try to compensate.
-    if len(macaddress) == 12:
+    # Check the MAC address format and try to compensate.
+    if len(MAC_address) == 12:
         pass
-    elif len(macaddress) == 12 + 5:
-         macaddress = macaddress.replace(macaddress[2], '')
+    elif len(MAC_address) == 12 + 5:
+        MAC_address = MAC_address.replace(MAC_address[2], '')
     else:
         raise ValueError('Incorrect MAC address format')
 
     # Pad the synchronization stream.
-    data = ''.join(['FFFFFFFFFFFF', macaddress * 20])
+    data = ''.join(['FFFFFFFFFFFF', MAC_address * 20])
     send_data = b''
 
     # Split up the hex values and pack.
     for i in range(0, len(data), 2):
         send_data = b''.join([send_data,
-                             struct.pack('B', int(data[i: i + 2], 16))])
+                              struct.pack('B', int(data[i: i + 2], 16))])
 
     # Broadcast it to the LAN.
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -31,7 +32,12 @@ def wake_on_lan(macaddress):
     sock.sendto(send_data, ('<broadcast>', 7))
 
 
-
 if __name__ == '__main__':
-    # Use macaddresses with any seperators.
-    wake_on_lan('F4:4D:30:6E:08:41')
+    with open("wol_config.yml", "r") as stream:
+        try:
+            conf = yaml.safe_load(stream)
+            for key in conf:
+                wake_on_lan(conf[key]['MAC_address'])
+                print(conf[key]['hostname'], 'should be awake now')
+        except yaml.YAMLError as exc:
+            print(exc)
